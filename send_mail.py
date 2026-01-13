ed3f0342db80267047f2340aca260580b54ca318
John <johnpythoncode@gmail.com>
2026-01-13 11:43:00 +0100

    Change favicon and use resend API for email

--- send_mail.py
+++ send_mail.py
@@ -1,22 +1,41 @@
-import smtplib
 import os
+import resend
 
+
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 class Email:
 
     def __init__(self):
-        self.MY_EMAIL = os.getenv("MY_EMAIL")
-        self.PASSWORD = os.getenv("PASSWORD")
-        self.SMTP_ADDRESS = os.getenv("SMTP_ADDRESS")
+        self.SENDER = os.getenv("SENDER")
         self.RECIPIENT = os.getenv("RECIPIENT")
+
+        if not all([self.SENDER, self.RECIPIENT, resend.api_key]):
+            raise RuntimeError("Email environment variables not set")
 
 
     def post_email(self, name, email, phone, message):
-        with smtplib.SMTP_SSL(self.SMTP_ADDRESS, 465) as connection:
-            connection.login(user=self.MY_EMAIL, password=self.PASSWORD)
-            connection.sendmail(
-                    from_addr=self.MY_EMAIL,
-                    to_addrs=self.RECIPIENT,
-                    msg=f"Subject: New Portfolio Connection!\n\n"
-                        f"Name: {name} \nEmail: {email} \nPhone: {phone} \nMessage: {message}"
-            )
+        """Using resend API to send an email """
+
+        try:
+            # Suppress PyCharm TypedDict type-checking warning for Resend params using noinspection
+            # noinspection PyTypeChecker
+            params: resend.Emails.SendParams = {
+                "from": f"Portfolio <{self.SENDER}>",
+                "to": [self.RECIPIENT],
+                "subject": "New Portfolio Connection!",
+                "html": f"""
+                    <p><strong>Name:</strong> {name}</p>
+                    <p><strong>Email:</strong> {email}</p>
+                    <p><strong>Phone:</strong> {phone}</p>
+                    <p><strong>Message:</strong> {message}</p>
+                """
+            }
+
+            # Send the email
+            response = resend.Emails.send(params)
+            return response
+
+        except Exception as e:
+            raise RuntimeError(f"Resend email failed: {e}")
+
